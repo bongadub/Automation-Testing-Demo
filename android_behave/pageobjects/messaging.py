a012de1112f55b08b47f46d3c3aa7ec28f6c9c1a
Samkelo-Kinetic <samkelo@kineticskunk.com>
2019-03-19 12:19:49 +0200

    updated

--- android_behave/pageobjects/messaging.py
+++ android_behave/pageobjects/messaging.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 u"""
 Copyright 2015 Telefónica Investigación y Desarrollo, S.A.U.
-This file is part of Toolium.
-<<<<<<< HEAD
+This file is part of Toolium
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -15,7 +14,6 @@ You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
 
->>>>>>> master
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -42,6 +40,5 @@ class MessagingPageObject(PageObject):
 		
 		self.recipient_lbl = self.driver.find_elements(By.XPATH, '//*/android.widget.EditText')[0].send_keys('0614206270')
 		self.msg_lbl = self.driver.find_elements(By.XPATH, '//*/android.widget.EditText')[1].send_keys('Hello there! This an automated text, please feel free not to reply . Team (KS)')
-
 		self.button = self.driver.find_elements(By.XPATH, '//*/android.widget.Button')[0].click()
 		time.sleep(3)
